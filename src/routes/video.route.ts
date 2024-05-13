1ee8e42008c29e9306985f57f714968511ad266e
jessdebrito <debrito.herself@gmail.com>
2024-05-13 20:24:40 -0300

    Update video.route.ts

--- src/routes/video.route.ts
+++ src/routes/video.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { VideoController } from "../controllers/video.controllers";
+import { IsVideoIdValid } from "../middleware/isVideoIdValid.middleware";
 
 export const videoRouter = Router();
 
@@ -8,5 +9,5 @@ const videoController = new VideoController();
 videoRouter.post("/", videoController.create);
 videoRouter.get("/", videoController.getMany);
 videoRouter.get("/:id", videoController.getOne);
-videoRouter.patch("/:id", videoController.update);
-videoRouter.delete("/:id", videoController.remove);
+videoRouter.patch("/:id", IsVideoIdValid.execute, videoController.update);
+videoRouter.delete("/:id", IsVideoIdValid.execute ,videoController.remove);
