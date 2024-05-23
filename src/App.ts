67079df37b06347fd0c78b44ad897ad314f9ddee
jessdebrito <debrito.herself@gmail.com>
2024-05-23 19:44:20 -0300

    helmet

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,11 @@
 import express, { json } from "express";
 import { videoRouter } from "./routes/video.route";
 import { HandleErrors } from "./middleware/handleErrors.middleware";
+import helmet from "helmet";
 
 export const app = express();
+
+app.use(helmet());
 
 app.use(json());
 
